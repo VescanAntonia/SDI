080ded7bc148478913b1101fed41e8d3deb0d435
Vescan_Antonia <91742401+VescanAntonia@users.noreply.github.com>
2023-04-02 22:05:48 +0300

    Update views.py

--- A1/snippets/views.py
+++ A1/snippets/views.py
@@ -19,7 +19,7 @@ from rest_framework_swagger.views import get_swagger_view
 schema_view = get_swagger_view(title='Pastebin API')
 
 urlpatterns = [
-    url(r'^$', schema_view)
+    url("schema/", schema_view)
 ]
 
 
